0ec2a89ea1708e90133fac7a747009e784d7554c
Administrator <lanhuaguizha@outlook.com>
2017-06-04 00:19:49 +0800

    Initial commit

---
+++ src/main/java/com/christian/ChristianServerApplication.java
@@ -0,0 +1,12 @@
+package com.christian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristianServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristianServerApplication.class, args);
+	}
+}
