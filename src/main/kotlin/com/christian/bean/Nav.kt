40471c971212710daf6bf2535a2340d4b6ac9016
lanhuaguizha <lanhuaguizha@outlook.com>
2018-03-04 00:31:23 +0800

    Simulate nav successful

--- src/main/kotlin/com/christian/bean/Nav.kt
+++ src/main/kotlin/com/christian/bean/Nav.kt
@@ -1,4 +1,3 @@
 package com.christian.bean
 
-data class Nav(val subtitle: String, val title: String)
-//data class Nav(val subtitle: String, val title: String, val content: String, val author: String)
+data class Nav(val subtitle: String, val title: String, val content: String, val author: String)
--- src/main/kotlin/com/christian/controller/NavController.kt
+++ src/main/kotlin/com/christian/controller/NavController.kt
@@ -5,26 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
-//@RequestMapping("/home")
-//fun getHome(): List<Home> {
-//    return Arrays.asList<T>(
-//            Home("God save me", "2017.6.4", "yong"),
-//            Home("God save me", "2017.6.4", "yong"),
-//            Home("God save me", "2017.6.4", "yong")
-//    )
-//}
-
-
 @RestController
 class NavController {
 
-    //    http://127.0.0.1:8080/hello
-    val home: List<Nav>
-        @RequestMapping("/home")
+    //    http://127.0.0.1:8080/nav
+    val navs: List<Nav>
+        @RequestMapping("/nav")
         get() = Arrays.asList<Nav>(
-                Nav("马太福音", "怜恤人的人有福了"),
-                Nav("马太福音", "贫穷的人有福了"),
-                Nav("马太福音", "温柔的人有福了")
+                Nav("马太福音", "怜恤人的人有福了","...","弟兄"),
+                Nav("马太福音", "贫穷的人有福了","...","姊妹"),
+                Nav("马太福音", "温柔的人有福了","...","弟兄"),
+                Nav("马太福音", "清心的人有福了","...","姊妹")
         )
 
 
