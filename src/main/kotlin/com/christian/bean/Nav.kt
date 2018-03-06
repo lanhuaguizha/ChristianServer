eb6f76e63e500e9cb117d87215ae6a169a7de446
paultao <paultao@pptv.com>
2018-03-06 19:46:54 +0800

    nav bean initialize

--- src/main/kotlin/com/christian/bean/Nav.kt
+++ src/main/kotlin/com/christian/bean/Nav.kt
@@ -1,3 +1,3 @@
 package com.christian.bean
 
-data class Nav(val subtitle: String, val title: String, val content: String, val author: String)
+data class Nav(val subtitle: String =  "副标题", val title: String = "标题", val content: String="内容", val relation: String="相关文章", val author: String="弟兄")
